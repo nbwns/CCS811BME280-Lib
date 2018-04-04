970848818068d3fc51cbac7495ff8eb9cf486c0f
Nicolas Bauwens <nicolas.bauwens@gmail.com>
2018-04-04 13:01:37 +0200

    Add files via upload

---
+++ CCS811BME280Sensor.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.I2c;
+
+
+namespace CCS811_BME280_Library
+{
+    public class CCS811BME280Sensor
+    {
+        
+
+        private I2cDevice I2C { get; }
+        internal const byte CCS811_I2C_ADDR = 0x5A;
+        internal const byte CCS811BME280_I2C_ADDR = 0x5B;
+
+        private const byte MEAS_MODE = 0x01;
+        private const byte MEAS_DRIVE_1SEC = 0x10;
+        private const byte MEAS_DRIVE_60SEC = 0x30;
+        private const byte APP_START = 0xF4;
+        private const byte HW_ID = 0x20;
+        private const byte STATUS = 0x00;
+        private const byte ERROR_ID = 0xE0;
+        private const byte SW_RESET = 0xFF;
+
+
+        public CCS811BME280Sensor(ref I2cDevice i2CDevice)
+        {
+            I2C = i2CDevice;
+
+        }
+
+        internal void Initialize()
+        {
+            var resetCommand = new byte[] { SW_RESET, 0x11, 0xE5, 0x72, 0x8A };
+            I2C.Write(resetCommand);
+
+            byte hwId = ReadDataByte(HW_ID);
+
+            if (hwId == 0x81)
+            {
+                byte status = ReadDataByte(STATUS);
+                Debug.WriteLine(status);
+                //16 is OK but application mode not started
+
+                //start application mode
+                var command = new byte[] { APP_START };
+                I2C.Write(command);
+
+                //if 255 : everything is OK - BUT ERROR flag to 1 => check error. should be 254
+                status = ReadDataByte(STATUS);
+                Debug.WriteLine(status);
+
+                //output the error code
+                HandleError();
+
+                //set the measurement to 60 seconds
+                WriteDataByte(MEAS_MODE, MEAS_DRIVE_1SEC);
+
+            }
+            else
+            {
+                HandleError();
+            }
+            
+           
+        }
+
+        private void HandleError()
+        {
+            byte errorId = ReadDataByte(ERROR_ID);
+
+            Debug.WriteLine("Error ID: " + errorId);
+        }
+
+        internal int[] ReadCO2TVOC()
+        {
+            //byte status = ReadDataByte(STATUS);
+
+            //Debug.WriteLine("status: "+ status);
+
+            var data = ReadDataBlock(0x02, 8);
+            var co2 = (data[0] << 8) | data[1];
+            var tvoc = (data[2] << 8) | data[3];
+            Debug.WriteLine("CO2:" + co2);
+            Debug.WriteLine("TVOC:" + tvoc);
+            return new int[] { co2, tvoc };
+        }
+
+        private void WriteDataByte(byte reg, byte val)
+        {
+            var command = new[] { reg, val };
+            I2C.Write(command);
+        }
+
+        private byte ReadDataByte(byte reg)
+        {
+            var command = new[] { reg };
+            var data = new byte[1];
+
+            I2C.WriteRead(command, data);
+
+            return data[0];
+        }
+
+        byte[] ReadDataBlock(byte reg, int len)
+        {
+            var command = new[] { reg };
+            var data = new byte[len];
+
+            I2C.WriteRead(command, data);
+
+            return data;
+        }
+    }
+}
---
+++ SensorHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace CCS811_BME280_Library
+{
+    public class SensorHelper
+    {
+        public enum Type
+        {
+            CCS811,
+            CCS811BME280
+        }
+
+        public struct CO2TVOC
+        {
+            public int CO2;
+            public int TVOC;
+            public CO2TVOC(int co2, int tvoc)
+            {
+                CO2 = co2;
+                TVOC = tvoc;
+            }
+        }
+
+        private I2cDevice _i2CDevice;
+
+        public CCS811BME280Sensor Sensor { get; private set; }
+
+        public SensorHelper(Type sensorType)
+        {
+            Initialize(sensorType);
+        }
+
+        private async void Initialize(Type sensorType)
+        {
+            await InitializeI2C(sensorType);
+            Sensor = new CCS811BME280Sensor(ref _i2CDevice);
+            Sensor.Initialize();
+        }
+
+        private async Task InitializeI2C(Type sensorType)
+        {
+            var addr = CCS811BME280Sensor.CCS811_I2C_ADDR;
+            if (sensorType == Type.CCS811BME280)
+            {
+                addr = CCS811BME280Sensor.CCS811BME280_I2C_ADDR;
+            }
+
+            var settings = new I2cConnectionSettings(addr);
+            settings.BusSpeed = I2cBusSpeed.StandardMode;
+            settings.SharingMode = I2cSharingMode.Shared;
+
+            var devices = await DeviceInformation.FindAllAsync(I2cDevice.GetDeviceSelector("I2C1"));
+
+            if (devices.Count == 0)
+            {
+                throw new Exception(sensorType.ToString() + " device not found");
+            }
+
+            _i2CDevice = await I2cDevice.FromIdAsync(devices[0].Id, settings);
+        }
+
+        public CO2TVOC ReadCO2TVOC()
+        {
+           var data = Sensor.ReadCO2TVOC();
+            return new CO2TVOC(data[0], data[1]);
+        }
+
+    }
+}
